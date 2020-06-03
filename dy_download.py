f0d8f64055c0ddf16cb9e8a51354a1e937945847
Evan Chang <evanc577@gmail.com>
2020-06-03 00:40:34 -0400

    Draw progress bar for downloads

--- dy_download.py
+++ dy_download.py
@@ -7,10 +7,37 @@ import json
 import re
 import requests
 import sys
+from tqdm import tqdm
+import io
+
 
 HEADERS = {
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0",
         }
+
+
+def download(url, what=None):
+    if what is not None:
+        print(f"Downloading {what}...")
+
+    r = requests.get(url, headers=HEADERS, stream=True)
+    if not r.ok:
+        print(f"could not download {what}")
+        sys.exit(1)
+
+    total_size = int(r.headers.get('content-length', 0))
+    block_size = 1024
+
+    t = tqdm(total=total_size, unit='iB', unit_scale=True)
+    f = io.BytesIO()
+    for data in r.iter_content(block_size):
+        t.update(len(data))
+        f.write(data)
+    t.close()
+
+    f.seek(0)
+    return f.read()
+
 
 # parse program arguments
 parser = argparse.ArgumentParser(description='Download TikTok videos without watermark')
@@ -41,32 +68,28 @@ else:
         output_file = None
 
 # find video url (watermarked)
-resp = requests.get(url, headers=HEADERS)
+content = download(url, what="webpage")
 regex = r'playAddr: "(?P<url>.+?)"'
-match = re.search(regex, resp.text)
+match = re.search(regex, content.decode())
 if match is None:
-    print("Could not download video")
+    print("Could not find video")
     sys.exit(1)
 video_url = match.group('url')
 
 # get non-watermarked url
-vid_pos = -1
-resp = requests.get(video_url, headers=HEADERS)
-vid_pos = resp.content.find(b'vid:')
+content = download(video_url, what="watermarked video")
+vid_pos = content.find(b'vid:')
 if vid_pos == -1:
     print("Could not extract vid")
     sys.exit(1)
-vid = resp.content[vid_pos+4 : vid_pos+36].decode("utf-8")
+vid = content[vid_pos+4 : vid_pos+36].decode("utf-8")
 
 # download non-watermarked file
-resp = requests.get(f"https://aweme.snssdk.com/aweme/v1/play/?video_id={vid}&improve_bitrate=1&ratio=1080p", headers=HEADERS)
-if not resp.ok or len(resp.content) == 0:
-    print("Could not download no watermark video")
-    sys.exit(1)
+content = download(f"https://aweme.snssdk.com/aweme/v1/play/?video_id={vid}&improve_bitrate=1&ratio=1080p", what="non-watermarked video")
 if output_file == None:
     output = f"{vid}.mp4"
 else:
     output = output_file
 with open(output, "wb") as f:
-    f.write(resp.content)
-print(f"Downloaded to {output}")
+    f.write(content)
+print(f"Downloaded video to {output}")
--- tt_download.py
+++ tt_download.py
@@ -7,10 +7,36 @@ import json
 import re
 import requests
 import sys
+from tqdm import tqdm
+import io
+
 
 HEADERS = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:74.0) Gecko/20100101 Firefox/74.1",
         }
+
+
+def download(url, what=None):
+    if what is not None:
+        print(f"Downloading {what}...")
+
+    r = requests.get(url, headers=HEADERS, stream=True)
+    if not r.ok:
+        print(f"could not download {what}")
+        sys.exit(1)
+
+    total_size = int(r.headers.get('content-length', 0))
+    block_size = 1024
+
+    t = tqdm(total=total_size, unit='iB', unit_scale=True)
+    f = io.BytesIO()
+    for data in r.iter_content(block_size):
+        t.update(len(data))
+        f.write(data)
+    t.close()
+
+    f.seek(0)
+    return f.read()
 
 # parse program arguments
 parser = argparse.ArgumentParser(description='Download TikTok videos without watermark')
@@ -41,31 +67,28 @@ else:
         output_file = None
 
 # find video url (watermarked)
-resp = requests.get(url, headers=HEADERS)
+content = download(url, what="webpage")
 regex = r'<script id="__NEXT_DATA__" type="application/json" crossorigin="anonymous">(?P<json>.+?)</script>'
-match = re.search(regex,resp.text)
+match = re.search(regex, content.decode())
 if match is None:
-    print("Could not download video")
+    print("Could not find video")
     sys.exit(1)
 video_url = json.loads(match.group('json'))['props']['pageProps']['videoData']['itemInfos']['video']['urls'][0]
 
 # get non-watermarked url
-resp = requests.get(video_url, headers=HEADERS)
-vid_pos = resp.content.find(b'vid:')
+content = download(video_url, what="watermarked video")
+vid_pos = content.find(b'vid:')
 if vid_pos == -1:
     print("Could not extract vid")
     sys.exit(1)
-vid = resp.content[vid_pos+4 : vid_pos + 36].decode("utf-8")
+vid = content[vid_pos+4 : vid_pos + 36].decode("utf-8")
 
 # download non-watermarked file
-resp = requests.get(f"https://api2.musical.ly/aweme/v1/playwm/?video_id={vid}&improve_bitrate=1&ratio=1080p", headers=HEADERS)
-if not resp.ok or len(resp.content) == 0:
-    print("Could not download no watermark video")
-    sys.exit(1)
+content = download(f"https://api2.musical.ly/aweme/v1/playwm/?video_id={vid}&improve_bitrate=1&ratio=1080p", what="non-watermarked video")
 if output_file == None:
     output = f"{vid}.mp4"
 else:
     output = output_file
 with open(output, "wb") as f:
-    f.write(resp.content)
-print(f"Downloaded to {output}")
+    f.write(content)
+print(f"Downloaded video to {output}")
